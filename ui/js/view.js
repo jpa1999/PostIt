615babe11ebdc81cb7ecea57a54f4d32d52084b3
Janne Ahvo <janne.ahvo@gmail.com>
2012-02-24 15:16:47 +0200

    UI progress

--- ui/js/view.js
+++ ui/js/view.js
@@ -21,11 +21,9 @@ var MainView = function( model ){
 	}
 	
 	this.changePage = function(){
-		//alert("changePAge")
 		( this.model.parent.checkHash() )? this.show( this.model.parent.checkHash() ) : this.show( "etusivu" );	
 		
-		_gaq.push(['_trackPageview',location.pathname + location.search  + location.hash]);
-		snoobi.trackPageView( "tehtavatulevaisuudessa" + location.pathname + location.search  + location.hash )
+		
 	}
 	
 	this.hideEverything = function(){
@@ -41,40 +39,21 @@ var MainView = function( model ){
 		var hash_status 	= hash_array[1]
 		var hash_parameter 	= hash_array[2]
 
-		//snoobi.trackPageView( location.pathname + location.search  + location.hash, "tehtavatulevaisuudessa", hash_target);
-				
 		this.hideEverything();
 		$("." +  hash_target ).show()
+
+		getLists();
 		
 		this.checkForErrors( hash_status, hash_parameter )
 		this.highlightNavi( hash_target )
-		this.changeIllustration( hash_target )
 		
-		if( hash_target == "kilpailutyo" && ( hash_status == "error" || hash_status == "facebook_auth"  ) ){
-			this.showVoting()
-		}
-		if( hash_status == "facebook_auth" ){
-			$("#vote_name").val( url_variables.get("name")  )
-			$("#vote_email").val( url_variables.get("email") )
-			$("#vote_facebook_id").val( url_variables.get("facebook_id") )
-			$("#voting_form").submit()
-		}
-			
-		if( hash_status == "vote_kiitos" ){
-			
-			alert( "Kiitos äänestäsi! Onnea arvontaan." )
-			/*$( "#dialog-message" ).dialog({
-				modal: true,
-				buttons: {
-					Ok: function() {
-						$( this ).dialog( "close" );
-					}
-				}
-			});*/
-		}
 	}
 	
-
+	this.getLists = function(){
+	
+		
+	}
+	
 	this.highlightNavi = function( target ){
 		$(".navigation ul li a").removeClass("selected_li")
 		$(".navigation ul li a." + target ).addClass("selected_li")
@@ -89,62 +68,6 @@ var MainView = function( model ){
 	this.initButtons = function(){
 	
 	}
-	
-	this.initEditTools = function(){
-		var parent = this
-		$(".image.item_edit").hide()	
-		$(".image .show_tools").click( function(){ parent.onEdit("image")  })
-		$(".image .save_edit").click( function(){ parent.onEditEnd("image")  })
-		$(".image_cancel").click( function(){
-			$( ".image.item_display").show()
-			$( ".image.item_edit").hide()
-		
-		} )
-		
-		
-		
-		$(".team_name.item_edit").hide()	
-		$(".team_name .show_tools").click( function(){ parent.onEdit("team_name")  })
-		$(".team_name .save_edit").click( function(){ parent.onEditEnd("team_name")  })
-		
-		$(".url.item_edit").hide()	
-		$(".url .show_tools").click( function(){ parent.onEdit("url")  })
-		$(".url .save_edit").click( function(){ parent.onEditEnd("url")  })
-		
-		$(".team.item_edit").hide()	
-		$(".team .show_tools").click( function(){ parent.onEdit("team")  })
-		$(".team .save_edit").click( function(){ parent.onEditEnd("team")  })
-		
-		$(".description.item_edit").hide()	
-		$(".description .show_tools").click( function(){ parent.onEdit("description")  })
-		$(".description .save_edit").click( function(){ parent.onEditEnd("description")  })
-		
-		$(".title.item_edit").hide()	
-		$(".title .show_tools").click( function(){ parent.onEdit("title")  })
-		$(".title .save_edit").click( function(){ parent.onEditEnd("title")  })
-		
-		$(".contact_details.item_edit").hide()	
-		$(".contact_details .show_tools").click( function(){ parent.onEdit("contact_details")  })
-		
-	}
-	
-	this.onEdit = function( target ){
-		$( "." + target + ".item_display").hide()
-		$( "." + target + ".item_edit").show()
-	}
-	
-	this.onEditEnd = function( target ){
-		$( "." + target + ".item_display").show()
-		$( "." + target + ".item_edit").hide()
-		
-		var jquery_path =  "." + target + ".item_edit .edit_field"
-		
-		$("#update_form .update_value").val( $( jquery_path ).val() )
-		$("#update_form .target").val( target )
-		$("#update_form").submit()
-		
-	}
-	
 	
 	this.init()
 	
